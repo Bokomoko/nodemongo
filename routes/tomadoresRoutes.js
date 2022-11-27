c642f21140bcb39a77e6787c44065ea4b73c7750
Joao Eurico <joao.eurico@live.com>
2022-11-27 14:17:54 -0300

    DELETE and PUT methods added

--- routes/tomadoresRoutes.js
+++ routes/tomadoresRoutes.js
@@ -54,5 +54,46 @@ tomadoresRouter.get('/:id', async (req, res) => {
   }
 })
 
+// this equivalent to DELETE to /tomadores/:id
+// delete a single tomador
+tomadoresRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const tomador = await Tomadores.findByIdAndDelete(id)
+    if (!tomador) {
+      res.status(404).json({ error: "Tomador not found" })
+      return
+    }
+    res.status(200).json(tomador)
+  }
+  catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+// this equivalent to PUT to /tomadores/:id
+// update a single tomador
+tomadoresRouter.put('/', async (req, res) => {
+  const tomador = { ...req.body }
+  if (!tomador._id) {
+    res.status(400).json({ error: "_id is required" })
+    return
+  }
+
+  try {
+    const result = await Tomadores.findByIdAndUpdate(tomador._id, tomador, { new: true })
+    if (!result) {
+      res.status(404).json({ error: "Tomador not found" })
+      return
+    }
+    res.status(200).json(result)
+  }
+  catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+
+
 
 export default tomadoresRouter
